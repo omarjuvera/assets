c868137d82547993c7523276b46726bfce41be5d
omarjuvera <omar.myspam@gmail.com>
2024-02-04 10:25:44 -0800

    Create Breadcrumbs.js

---
+++ portfolios/scripts/Breadcrumbs.js
@@ -0,0 +1,26 @@
+// Breadcrumbs.js
+	// rev. 2024-02-03
+const baseUrl = `https://${username}.github.io/`;
+const repoUrl = `${baseUrl}${repo}`;
+
+const breadcrumbDiv = document.getElementById('breadcrumbs');
+const breadcrumbSeparator = ' > ';
+const breadcrumbPath = window.location.pathname.split('/').slice(2, -1);
+
+const homeIcon = '🏠'; // You can replace this with an actual home icon
+
+function createBreadcrumbLink(path, index) {
+	const formattedPath = path.replace(/-/g, ' ').replace(/%20/g, ' ').replace(/\b\w/g, l => l.toUpperCase()); // Convert to title case
+	const pathUrl = `${repoUrl}/${breadcrumbPath.slice(0, index + 1).join('/')}/`;
+
+	return `<a href="${pathUrl}">${formattedPath}</a>`;
+}
+
+const breadcrumbArray = [`<a href="${repoUrl}">${homeIcon} ${repo}</a>`];
+
+breadcrumbPath.forEach((path, index) => {
+	breadcrumbArray.push(createBreadcrumbLink(path, index));
+});
+
+const breadcrumbTrail = breadcrumbArray.join(breadcrumbSeparator);
+breadcrumbDiv.innerHTML = breadcrumbTrail;
